0b97a97c76b62c986a3b4479112d49568508adfe
Yujian(Ken) He <55962680+Kennnnn774@users.noreply.github.com>
2021-03-29 17:04:00 -0700

    Updates

---
+++ precache-manifest.d796bb3ca98777ff873f530e4e96502e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d6e172c59ec6af46669a6ad2e20b17e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cde1262b4680193376e",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cde1262b4680193376e",
+    "url": "/static/js/main.3e0f3ed9.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
