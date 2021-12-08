57f2f9759d31ec52d0b3ecfb06da282d10844581
Yujian(Ken) He <55962680+Kennnnn774@users.noreply.github.com>
2021-12-07 16:17:28 -0800

    Updates

---
+++ precache-manifest.f42f5264a5e8da0d8e51065f40d17bff.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9bb412991eed002d3524eb6fe9afe57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6638ac10c4d26fb8cb06",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "54d8552a916a81685efb",
+    "url": "/static/js/2.25efede7.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.25efede7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6638ac10c4d26fb8cb06",
+    "url": "/static/js/main.63519fdd.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "ad88d1121c2233aef2ed722ba21fa3e8",
+    "url": "/static/media/CBS.ad88d112.png"
+  },
+  {
+    "revision": "4cac4984dde6c17bb42966e4302cef66",
+    "url": "/static/media/ken_suit.4cac4984.jpg"
+  },
+  {
+    "revision": "e05a4fc53ebe7d026b999b98f976b2a6",
+    "url": "/static/media/resume.e05a4fc5.pdf"
+  }
+]);
